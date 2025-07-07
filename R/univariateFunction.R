1b7669aa0f65dab6c2fdb14c72a02a48860f223a
Daniele <daniele.tambone@icloud.com>
2025-07-07 18:34:15 +0200

    almost finished #1

--- R/univariateFunction.R
+++ R/univariateFunction.R
@@ -33,3 +33,21 @@ plotHistModel <- function(variable, model) {
   histModel <- histDist(variable, model$family[1] , density=TRUE, line.col=c(1,1), line.ty=c(1,2))
   histModel
 }
+
+plotHistModelMix <- function(variable, model) {
+  colors <- c("red3", "green3")
+  x_vals <- seq(min(variable), max(variable), length.out = 500)
+  
+  hist(variable, breaks = 30, probability = T, col = "white", border = "black",
+       main = "Gamma Mixture Model K = 2",
+       xlab = "Values", ylab = "Density")
+  
+  for (i in 1:length(model$pi)) {
+    lines(x_vals, model$pi[i] * dgamma(x_vals, shape = model$alpha[i], rate = model$lambda[i]), col = colors[i], lwd = 2)
+  }
+  
+  overall_density <- rowSums(sapply(1:length(model$pi), function(i) {
+    model$pi[i] * dgamma(x_vals, shape = model$alpha[i], rate = model$lambda[i])
+  }))
+  lines(x_vals, overall_density, col = "black", lwd = 2, lty = 2)
+}
